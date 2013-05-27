4ab7571eb0de93dc85a6dcc90e7d80812953690d
Galen Palmer <galenp@gmail.com>
2013-05-26 23:34:01 -0400

    adding default mailer path for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,8 @@ Ehr::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  
+  config.action_mailer.default_url_options = { :host => 'ec2-23-21-136-195.compute-1.amazonaws.com' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
